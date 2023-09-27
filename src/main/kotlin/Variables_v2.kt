8e5cf03ade2c9e1ec7b3e25c3e51a355523d7a31
Max Lagunes <mlagunesm2000@alumno.ipn.mx>
2023-09-27 12:25:02 -0600

    Práctica: Variables y Tipos de datos (TEAMS)

---
+++ src/main/kotlin/Variables_v2.kt
@@ -0,0 +1,7 @@
+const val PI = 3.14159
+fun main(){
+    val radio = 5
+    val numeroFloat : Float = radio.toFloat()
+    val resultado = 2 * PI * numeroFloat
+    println("El perímetro del círculo es : $resultado")
+}
